5356025f29eb2fcd2ac6eb6c03eb894a375b6691
SEQ1 System <seq1@mndwave.com>
2025-07-04 10:03:01 +0000

    CANONICAL: Fixed blockheight 903951 in hidden div

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Space_Mono, Poppins } from 'next/font/google'
 import './globals.css'
 import VersionIndicator from "@/components/version-indicator"
 import { AuthProvider } from '../lib/auth-context'
-/* SEQ1_BLOCKHEIGHT:903951 */
+
 
 // Space Mono for body text (regular weight)
 const spaceMono = Space_Mono({ 
@@ -35,7 +35,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${spaceMono.variable} ${poppins.variable}`}>
-      <body className={spaceMono.className}>
+      <body className={spaceMono.className}><div style={{"display":"none"}}>SEQ1_BLOCKHEIGHT:903951</div><div style={{display:"none"}}>SEQ1_BLOCKHEIGHT:903951</div>
         <AuthProvider>
           <VersionIndicator />
           {children}
