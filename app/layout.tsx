a2add622b2f0445413d18d6b1cd8ca6a7ee85d76
SEQ1 System <seq1@mndwave.com>
2025-07-04 09:59:25 +0000

    CANONICAL: Deploy blockheight 903951

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Space_Mono, Poppins } from 'next/font/google'
 import './globals.css'
 import VersionIndicator from "@/components/version-indicator"
 import { AuthProvider } from '../lib/auth-context'
+/* SEQ1_BLOCKHEIGHT:903951 */
 
 // Space Mono for body text (regular weight)
 const spaceMono = Space_Mono({ 
